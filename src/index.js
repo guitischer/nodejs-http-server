bc0932ff665de063f9b6207d1e2583ace4e4231f
guitischer <guitischer@hotmail.com.br>
2022-07-07 14:44:51 -0300

    added getUserById route

--- src/index.js
+++ src/index.js
@@ -7,13 +7,26 @@ const server = http.createServer((request, response) => {
   const parsedUrl = new URL(`http://localhost:3000${request.url}`);
   console.log(`Request Method ${request.method} | Endpoint: ${parsedUrl}`);
 
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+    routeObj.endpoint === pathname && routeObj.method === request.method
   ));
 
   if (route) {
     request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = {id}
+
     route.handler(request, response);
+    
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html'});
     response.end(`Cannot ${request.method} ${parsedUrl}`);
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
-const { listUsers } = require("./controllers/UserController");
+const { listUsers, getUserById } = require("./controllers/UserController");
 
 module.exports = [
   {
     endpoint: '/users',
     method: 'GET',
     handler: listUsers
+  },
+  {
+    endpoint: '/users/:id',
+    method: 'GET',
+    handler: getUserById
   },
 ]
